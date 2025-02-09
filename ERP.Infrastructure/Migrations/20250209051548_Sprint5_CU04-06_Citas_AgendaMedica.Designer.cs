7f22e658ddd0100ce9d169bfed35c85fd166fa94
Hans Kinderman <hanskch1993@hotmail.com>
2025-02-09 00:27:41 -0500

    DOCKER

--- ERP.Infrastructure/Migrations/20250209051548_Sprint5_CU04-06_Citas_AgendaMedica.Designer.cs
+++ ERP.Infrastructure/Migrations/20250209051548_Sprint5_CU04-06_Citas_AgendaMedica.Designer.cs
@@ -4,6 +4,7 @@ using ERP.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ERP.Infrastructure.Migrations
 {
     [DbContext(typeof(ContextoDatos))]
-    partial class ContextoDatosModelSnapshot : ModelSnapshot
+    [Migration("20250209051548_Sprint5_CU04-06_Citas_AgendaMedica")]
+    partial class Sprint5_CU0406_Citas_AgendaMedica
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ERP.Infrastructure/Migrations/20250209051548_Sprint5_CU04-06_Citas_AgendaMedica.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERP.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Sprint5_CU0406_Citas_AgendaMedica : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Estado",
+                table: "Facturas",
+                type: "int",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Estado",
+                table: "Facturas",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldMaxLength: 50);
+        }
+    }
+}
